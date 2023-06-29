df3f0e534c0fc6f7c45ab806cdb6acae90c2a347
Kicknee <daniszewski21@gmail.com>
2023-06-29 19:27:13 +0200

    removing scroll event when modal is on

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ document.querySelectorAll(".uslugi-btn").forEach((element) => {
       .querySelector(`.modal.${event.target.classList[1]}`)
       .classList.add("active");
 
+    document.body.classList.add("stop-scrolling");
+
     document
       .querySelector(`.close-btn.${event.target.classList[1]}`)
       .addEventListener("click", () => {
         document
           .querySelector(`.modal.${event.target.classList[1]}`)
           .classList.remove("active");
+
+        document.body.classList.remove("stop-scrolling");
       });
 
     // document.querySelectorAll(`.modal .info`).forEach((el) => {
@@ -25,5 +29,8 @@ document.querySelectorAll(".uslugi-btn").forEach((element) => {
 window.onclick = (event) => {
   if (event.target.classList.contains("modal")) {
     event.target.classList.remove("active");
+    document.body.classList.remove("stop-scrolling");
   }
 };
+
+document.querySelector("#curr_date").textContent = new Date().getFullYear();
